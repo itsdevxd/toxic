7cbabccfe8c7f93b2d2b17f06d2f373a6626df30
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-07-23 21:02:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,13 +89,15 @@ START_VIDS = [
     "https://telegra.ph/file/9b7e1b820c72a14d90be7.mp4",
     "https://telegra.ph/file/72f349b1386d6d9374a38.mp4",
     "https://telegra.ph/file/a4d90b0cb759b67d68644.mp4",
-    "https://ar-hosting.pages.dev/1753090765686.mp4",
+    "https://ar-hosting.pages.dev/1753090765686.mp4"
 ]
 
 STICKERS = [
-    "CAACAgUAAx0Cd6nKUAACASBl_rnalOle6g7qS-ry-aZ1ZpVEnwACgg8AAizLEFfI5wfykoCR4h4E",
-    "CAACAgUAAx0Cd6nKUAACATJl_rsEJOsaaPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ"
+    "CAACAgUAAxkBAAEO-r9ofhl9UwhDzvXyDxohWDCxTvWPUwAC4gsAApy2kFcqc-w_yX7BbTYE",
+    "CAACAgUAAxkBAAEO-sFofhmDkIfsS_SeX7xAt0rlWew1LAACsw4AArzvmFf3UP-QMkPiMTYE",
+
 ]
+
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
