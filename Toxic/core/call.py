c2dbe721d6b7332ddc0fe80bbcb076dc22a62e66
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-07-24 00:08:44 +0530

    Update call.py

--- Toxic/core/call.py
+++ Toxic/core/call.py
@@ -55,7 +55,7 @@ app = Client("ToxicAss1",
              api_id=API_ID, 
              api_hash=API_HASH, 
              session=session)
-             app.run()
+             Dev.run()
 
 
 class Call(PyTgCalls):
